fa42bcb3f648610bf9ea3b2f452688b0b3b15ccf
Adri Priego <priegolucena@gmail.com>
2024-03-05 17:01:52 +0100

    dia 2

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const mongoose = require("mongoose")
+const Student = require("./models/Student.model")
+const Cohort = require("./models/Cohort.model")
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -12,6 +15,12 @@ const students = require("./students.json");
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 const cors = require("cors");
+
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -35,10 +44,26 @@ app.get("/docs", (req, res) => {
 });
 
 app.get("/api/cohorts", (req, res) => {
-  res.json(cohorts);
+  Cohort.find()
+  .then((cohorts) => {
+    console.log("todos los cohorts", cohorts)
+    res.json(cohorts);
+  })
+  .catch((error) => {
+    console.log("error", error)
+    res.status(500).json({error: "fallo al recibir los cohorts"})
+  })
 });
 app.get("/api/students", (req, res) => {
-  res.json(students);
+  Student.find()
+  .then((students) => {
+    console.log("todos los estudiantes", students)
+    res.json(students);
+  })
+  .catch((error) => {
+    console.log("error", error)
+    res.status(500).json({error: "fallo al recibir los estudiantes"})
+  })
 });
 
 // START SERVER
